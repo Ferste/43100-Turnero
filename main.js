c5c881f4660baab7ae382a4371f3106aab5cef62
fernando stefoni <fer.st1995@gmail.com>
2023-07-16 20:02:10 -0300

    Turnero version final 12.3.4

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
 // Funcion para mostrar un mensaje de usuario valido
 function mostrarMensajeExito(mensaje) {
-    const mensajeElement = document.createElement('p');
-    mensajeElement.textContent = mensaje;
-    mensajeElement.classList.add('text-success');
-    document.body.appendChild(mensajeElement);
+    Swal.fire({
+        icon: 'success',
+        title: '¡Felicitaciones!',
+        text: mensaje,
+    });
 }
 
 // Funcion para mostrar un mensaje de error
 function mostrarMensajeError(mensaje) {
-    const mensajeElement = document.createElement('p');
-    mensajeElement.textContent = mensaje;
-    mensajeElement.classList.add('text-danger');
-    document.body.appendChild(mensajeElement);
+    Swal.fire({
+        icon: 'error',
+        title: '¡Error!',
+        text: mensaje,
+    });
+}
+
+// Funcion para cargar los servicios desde el archivo JSON 
+function cargarServicios() {
+    fetch('servicios.json')
+        .then(response => response.json())
+        .then(data => {
+            const serviciosList = document.getElementById('serviciosList');
+            serviciosList.innerHTML = '';
+            data.forEach(servicio => {
+                const li = document.createElement('li');
+                li.classList.add('list-group-item');
+                const div = document.createElement('div');
+                div.classList.add('form-check');
+                const input = document.createElement('input');
+                input.type = 'checkbox';
+                input.name = 'servicio';
+                input.value = servicio.id;
+                input.dataset.precio = servicio.precio;
+                input.classList.add('form-check-input');
+                const label = document.createElement('label');
+                label.classList.add('form-check-label');
+                label.textContent = servicio.nombre;
+                div.appendChild(input);
+                div.appendChild(label);
+                li.appendChild(div);
+                serviciosList.appendChild(li);
+            });
+        })
+        .catch(error => {
+            console.error('Error al cargar los servicios:', error);
+        });
 }
 
 // Funcion para validar el usuario
@@ -19,30 +53,45 @@ function validarUsuario() {
     const usuarioInput = document.getElementById('usuario');
     const usuario = usuarioInput.value.trim();
 
-    if (usuario === '') {
-        mostrarMensajeError('Debes ingresar un nombre de usuario valido.');
+    const patenteInput = document.getElementById('patente');
+    const patente = patenteInput.value.trim();
+
+    if (usuario === '' || patente === '') {
+        mostrarMensajeError('Debes ingresar un nombre de usuario y una patente válidos.');
         return false;
     }
+
+    // Validar la patente con las expresiones regulares, la ñ no esta incluida en argetina para las patentes antiguas
+    const regexPatente1 = /^[A-Za-zÑñ]{3}\d{3}$/;
+    const regexPatente2 = /^[A-Za-z]{2}\d{3}[A-Za-z]{2}$/;
+    if (!regexPatente1.test(patente) && !regexPatente2.test(patente)) {
+        mostrarMensajeError('La patente ingresada no es válida.');
+        return false;
+    }
+
     localStorage.setItem('usuario', usuario);
+    localStorage.setItem('patente', patente);
 
     mostrarMensajeExito('¡Bienvenido, ' + usuario + '!');
+    document.getElementById('turnoSection').style.display = 'block';
+    document.getElementById('loginSection').style.display = 'none';
     return true;
 }
 
-// Evento submit del formulario de inicio de sesion
+// Evento submit del formulario de inicio de sesión
 const loginForm = document.getElementById('loginForm');
 loginForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Evitar el envio del formulario
-
-    if (validarUsuario()) {
-            mostrarMensajeExito('Usuario valido. Acceso permitido.');
-
-    // Ocultar formulario de inicio de sesion
-    loginForm.style.display = 'none';
-    }
+    event.preventDefault(); // Evitar el envío del formulario
+    //aplicacion de ternario 
+    validarUsuario() ? (
+            mostrarMensajeExito('Usuario válido. Acceso permitido.'),
+            loginForm.style.display = 'none',
+            cargarServicios()
+        )
+        : null;
 });
 
-// Funcin para programar un turno
+// Función para programar un turno
 function programarTurno() {
     const diaInput = document.getElementById('dia');
     const mesInput = document.getElementById('mes');
@@ -53,7 +102,15 @@ function programarTurno() {
     const hora = parseInt(horaInput.value);
 
     if (isNaN(dia) || isNaN(mes) || isNaN(hora) || dia < 1 || dia > 31 || mes < 1 || mes > 12 || hora < 8 || hora > 12) {
-        mostrarMensajeError('La fecha y hora ingresadas no son validas.');
+        mostrarMensajeError('La fecha y hora ingresadas no son válidas.');
+        return;
+    }
+
+    const fechaTurno = new Date(2023, mes - 1, dia, hora, 0, 0); // Crear objeto Date con la fecha seleccionada
+    const fechaActual = new Date(); // Obtener la fecha actual
+
+    if (fechaTurno < fechaActual) {
+        mostrarMensajeError('No se puede programar un turno en una fecha anterior a la actual.');
         return;
     }
 
@@ -64,23 +121,20 @@ function programarTurno() {
         mostrarMensajeError('Debes seleccionar al menos un servicio.');
         return;
     }
-    
-    let mensaje = 'Se realizaran los siguientes servicios:\n\n';
+
+    let mensaje = 'Se realizarán los siguientes servicios:\n\n';
     serviciosSeleccionados.forEach(servicio => {
         const nombreServicio = servicio.parentNode.textContent.trim();
         mensaje += `${nombreServicio}\n`;
     });
     mensaje += `\nValor total: $${total}`;
 
-    //no olvidar eliminar
-    //mostrarMensajeExito(mensaje);
-
     // Obtener la sucursal seleccionada
     const sucursalSelect = document.getElementById('sucursal');
     const sucursal = sucursalSelect.options[sucursalSelect.selectedIndex].text;
 
     // Obtener la fecha y hora del turno
-    const fechaHoraTurno = `${dia}/${mes} de 2023, ${hora}:00 hs`;
+    const fechaHoraTurno = `${dia}/${mes}/2023, a las ${hora}:00 hs`;
 
     // Guardar los datos del turno
     const turno = {
@@ -90,15 +144,32 @@ function programarTurno() {
     };
     localStorage.setItem('turno', JSON.stringify(turno));
 
-    // Mostrar mensaje 
-    const mensajeConfirmacion = `Se genero un turno en la sucursal ${sucursal} para el ${fechaHoraTurno}. Servicios seleccionados: ${mensaje}`;
+    // Mostrar mensaje de confirmación
+    const mensajeConfirmacion = `Se generó un turno en la sucursal ${sucursal} para la fecha ${fechaHoraTurno}. ${mensaje}`;
     mostrarMensajeExito(mensajeConfirmacion);
+    document.getElementById('turnoSection').style.display = 'block';
 }
 
-// Evento submit del formulario de programacion de turno
+// Restablecer la página después de programar el turno
+function restablecerPagina() {
+    // Mostrar el formulario de inicio de sesión y ocultar el formulario de turno
+    document.getElementById('loginSection').style.display = 'block';
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('turnoSection').style.display = 'none';
+
+    // Restablecer los valores de los campos de usuario y patente
+    document.getElementById('usuario').value = '';
+    document.getElementById('patente').value = '';
+
+    // Eliminar el turno almacenado en el local storage
+    localStorage.removeItem('turno');
+}
+
+// Evento submit del formulario de programación de turno
 const turnoForm = document.getElementById('turnoForm');
 turnoForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Evitar el envio del formulario
+    event.preventDefault(); // Evitar el envío del formulario
 
     programarTurno();
+    restablecerPagina();
 });
